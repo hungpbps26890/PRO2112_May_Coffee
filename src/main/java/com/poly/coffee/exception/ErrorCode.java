200fee691e4210283fb9e91a797ee530a892a187
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2024-05-27 22:06:31 +0700

    Add GlobalException, AppException and ErrorCode

---
+++ src/main/java/com/poly/coffee/exception/ErrorCode.java
@@ -0,0 +1,23 @@
+package com.poly.coffee.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(999, "Uncategorized exception"),
+    USER_EXISTED(1001, "User existed"),
+    USER_NOT_FOUND(1002, "User not found"),
+    INVALID_PASSWORD(1003, "Password must be at least 8 characters"),
+    CATEGORY_NOT_FOUND(1004, "Category not found"),
+    NOT_EMPTY_CATEGORY_NAME(1005, "Category name must be not empty"),
+    DRINK_NOT_FOUND(1004, "Drink not found")
+    ;
+
+    private final int code;
+    private final String message;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/poly/coffee/exception/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.poly.coffee.exception;
+
+import com.poly.coffee.dto.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Objects;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    ResponseEntity<ApiResponse<Object>> handlingRuntimeException(Exception exception) {
+        ApiResponse<Object> apiResponse = new ApiResponse<>();
+
+        apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
+        apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
+
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AppException.class)
+    ResponseEntity<ApiResponse<Object>> handlingAppException(AppException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+
+        ApiResponse<Object> apiResponse = new ApiResponse<>();
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<ApiResponse<Object>> handlingMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        String enumKey = Objects.requireNonNull(exception.getFieldError()).getDefaultMessage();
+
+        ErrorCode errorCode = ErrorCode.valueOf(enumKey);
+
+        ApiResponse<Object> apiResponse = new ApiResponse<>();
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+}
